5c91139f6afccbd224cffce468707e5038d8dfd6
priyanshunitr <sahupriyanshu2006@gmail.com>
2025-10-26 12:31:01 +0530

    26-10-25

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -3,20 +3,32 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 // coding.forEach( function (val){
 //     console.log(val);
 // } )
+// js
+// ruby
+// java
+// python
+// cpp
 
 // coding.forEach( (item) => {
 //     console.log(item);
 // } )
+//same as above
 
 // function printMe(item){
 //     console.log(item);
 // }
+//same as above
 
 // coding.forEach(printMe)
 
 // coding.forEach( (item, index, arr)=> {
 //     console.log(item, index, arr);
 // } )
+// js 0 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// ruby 1 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// java 2 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// python 3 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// cpp 4 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
 
 const myCoding = [
     {
